eb685fbbc5ca7c26216f2e93745ca9e99ed8c33c
DajanaIsaku <isaku.dajana@gmail.com>
2025-10-11 22:12:37 +0200

    Initial Commit

---
+++ MobilneApp/MobilneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobilneAppApp.swift
+//  MobilneApp
+//
+//  Created by Dajana Isaku on 11. 10. 2025..
+//
+
+import SwiftUI
+
+@main
+struct MobilneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
